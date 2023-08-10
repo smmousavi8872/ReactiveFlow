e5f12edd84eeb097ee260323cf2d0f0485b589ea
mo.mousavi <s.m.mousavi1993@gmail.com>
2023-08-10 06:51:56 +0330

    reafactor: id field added to data class.

--- app/src/main/java/io/github/smmousavi8872/event/MessageColdEvent.kt
+++ app/src/main/java/io/github/smmousavi8872/event/MessageColdEvent.kt
@@ -3,4 +3,4 @@ package io.github.smmousavi8872.event
 import io.github.smmousavi8872.reactiveflow.ColdEventFlow
 
 
-data class MessageColdEvent(var message: String) : ColdEventFlow()
+data class MessageColdEvent(val id: Int = 0, var message: String) : ColdEventFlow()
